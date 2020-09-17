edb221f05ee0237732e849ba7a83942a72c9da09
gonzalez <gonzalezmartinez@ucsb.edu>
2020-09-16 19:19:06 -0700

    9/16

--- octopus_arm.py
+++ octopus_arm.py
@@ -8,7 +8,15 @@ import os
 physicsClient = p.connect(p.GUI)
 
 #This sets the plane (floor). 
-planeId = p.loadURDF(os.path.join(pda.getDataPath(),"plane100.urdf"))
+#planeId = p.loadURDF(os.path.join(pda.getDataPath(),"plane100.urdf"))
+#planeId = p.loadURDF("plane100.urdf")
+#planeId = p.loadURDF(os.path.join(pda.getDataPath(),"octopus.urdf"))
+#there are 2 options, either get the urdf from my directory or transfer those files to the pda directory
+#p.setAdditionalSearchPath("/home/gonzalez/github/octopus") #indexes where the computer should/can look for a file
+#planeId = p.loadURDF("octopus.urdf") #option 1 get the urdf from my directory
+
+planeId = p.loadURDF(os.path.join(pda.getDataPath(),"edgarURDF/plane100.urdf")) #option 2 transfer those urdf files to the pda directory
+
 
 #Sets the gravity downward, (x,y,z)
 p.setGravity(0,0,-10)
